cbeed06b17d58aac9b68741853007e179ad35e8b
Hendrik López <hendriklopez2106@gmail.com>
2023-04-11 17:29:50 -0500

    se agrega metodo para eliminar nodos y relaciones; se agrega metodo para agregar relaciones

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,16 +12,34 @@ public class DemoApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(DemoApplication.class, args);
-        Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("username", "12345678"));
+        Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "12345678"));
         Session session = driver.session();
+        //addLugar(session, "casa", 40, 1920);
+        //deleteLugar(session, "casa");
+        //addRelacion(session, "Casa 0", "Casa 2", "Conduce", 100);
+        session.close();
+        driver.close();
 
     }
 
-    public void addLugar(Session session, String name, int height, int year) {
-        Result result = session.run("CREATE (lugar2:Lugar {nombre: " + name + ", altura: "+ height +", año: " + year + "})");
+    public static void addLugar(Session session, String name, int height, int year) {
+        Result result = session.run("CREATE (lugar:Lugar {nombre: '" + name + "', altura: "+ height +", año: " + year + "})");
         while (result.hasNext()) {
             Record record = result.next();
-            System.out.println(record.get("nombre").asString());
+        }
+    }
+
+    public static void addRelacion(Session session, String name1, String name2, String relation, int distance) {
+        Result result = session.run("MATCH (a:Lugar), (b:Lugar) WHERE a.nombre = '" + name1 + "' AND b.nombre = '"+name2+"' CREATE (a)-[r:"+relation+" {distancia: "+distance+"}]->(b) RETURN type(r)");
+        while (result.hasNext()) {
+            Record record = result.next();
+        }
+    }
+
+    public static void deleteLugar(Session session, String name) {
+        Result result = session.run("MATCH (n:Lugar {nombre: '"+name+"'}) DETACH DELETE n");
+        while (result.hasNext()) {
+            Record record = result.next();
         }
     }
 
