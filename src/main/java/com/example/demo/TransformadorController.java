48d83e73e8f2ee708d673585595623accebc833c
Hendrik López <hendriklopez2106@gmail.com>
2023-05-09 09:23:53 -0500

    Avances CRUD

--- src/main/java/com/example/demo/TransformadorController.java
+++ src/main/java/com/example/demo/TransformadorController.java
@@ -1,4 +1,5 @@
 package com.example.demo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
@@ -13,23 +14,35 @@ public class TransformadorController {
         this.transformadorRepository = transformadorRepository;
     }
 
-    @PostMapping
-    public Transformador crearTransformador(@RequestBody Transformador transformador) {
-        return transformadorRepository.save(transformador);
+    @GetMapping("/{id}")
+    public Transformador getTransformadorById(@PathVariable Long id) {
+        return transformadorRepository.findById(id).orElse(null);
     }
 
-    @GetMapping
-    public List<Transformador> obtenerTransformadores() {
+    @GetMapping("/")
+    public List<Transformador> getAllTransformadores() {
         return transformadorRepository.findAll();
     }
 
-    @GetMapping("/{id}")
-    public Optional<Transformador> obtenerTransformadorPorId(@PathVariable Long id) {
-        return transformadorRepository.findById(id);
+    @PostMapping("/")
+    public Transformador createTransformador(@RequestBody Transformador transformador) {
+        return transformadorRepository.save(transformador);
+    }
+
+    @PutMapping("/{id}")
+    public Transformador updateTransformador(@PathVariable Long id, @RequestBody Transformador transformador) {
+        Transformador existingTransformador = transformadorRepository.findById(id).orElse(null);
+        if (existingTransformador == null) {
+            return null;
+        }
+        existingTransformador.setCapacidad(transformador.getCapacidad());
+        existingTransformador.setEstado(transformador.getEstado());
+        existingTransformador.setTipo(transformador.getTipo());
+        return transformadorRepository.save(existingTransformador);
     }
 
     @DeleteMapping("/{id}")
-    public void eliminarTransformadorPorId(@PathVariable Long id) {
+    public void deleteTransformador(@PathVariable Long id) {
         transformadorRepository.deleteById(id);
     }
 }
