e32c789f5a684cd5402a9cef04417226a062f895
Hendrik López <hendriklopez2106@gmail.com>
2023-05-14 21:44:17 -0500

    Se agregan metodos para crear y eliminar relaciones en el controlador de la clase Transformador

--- src/main/java/com/example/demo/EstacionRepository.java
+++ src/main/java/com/example/demo/EstacionRepository.java
@@ -6,6 +6,10 @@ import org.springframework.data.neo4j.repository.query.Query;
 import java.util.List;
 
 public interface EstacionRepository extends Neo4jRepository<Estacion, Long> {
+    // Obtener transformadores conectados a una estación
     @Query("MATCH (e:Estacion)-[:Conecta]->(c:Estacion {nombre: $nombre}) RETURN e")
     List<Estacion> getEstacionesConectadas(String nombre);
+    // Obtener las estaciones a las que pertenece un transformador
+    @Query("MATCH (t:Transformador)-[:Pertenece_a]->(e:Estacion) WHERE id(t) = $transformadorId RETURN e")
+    Estacion getEstacionPertenece(Long transformadorId);
 }
--- src/main/java/com/example/demo/TransformadorController.java
+++ src/main/java/com/example/demo/TransformadorController.java
@@ -9,9 +9,14 @@ import java.util.Optional;
 public class TransformadorController {
 
     private final TransformadorRepository transformadorRepository;
+    private final ClienteRepository clienteRepository;
 
-    public TransformadorController(TransformadorRepository transformadorRepository) {
+    private final EstacionRepository estacionRepository;
+
+    public TransformadorController(TransformadorRepository transformadorRepository, ClienteRepository clienteRepository, EstacionRepository estacionRepository) {
         this.transformadorRepository = transformadorRepository;
+        this.clienteRepository = clienteRepository;
+        this.estacionRepository = estacionRepository;
     }
 
     @GetMapping("/{id}")
@@ -45,4 +50,46 @@ public class TransformadorController {
     public void deleteTransformador(@PathVariable Long id) {
         transformadorRepository.deleteById(id);
     }
+
+    @PostMapping("/{id}/alimentar/{clienteId}")
+    public Transformador alimentarCliente(@PathVariable Long id, @PathVariable Long clienteId) {
+        Transformador transformador = transformadorRepository.findById(id).orElse(null);
+        Cliente cliente = clienteRepository.findById(clienteId).orElse(null);
+        if (transformador != null && cliente != null) {
+            transformador.getClientesAlimentados().add(cliente);
+            transformadorRepository.save(transformador);
+        }
+        return transformador;
+    }
+    @DeleteMapping("/{id}/desalimentar/{clienteId}")
+    public Transformador desalimentarCliente(@PathVariable Long id, @PathVariable Long clienteId) {
+        Transformador transformador = transformadorRepository.findById(id).orElse(null);
+        Cliente cliente = clienteRepository.findById(clienteId).orElse(null);
+        if (transformador != null && cliente != null) {
+            transformador.getClientesAlimentados().remove(cliente);
+            transformadorRepository.save(transformador);
+        }
+        return transformador;
+    }
+    @PostMapping("/{id}/pertenecer_a/{estacionId}")
+    public Transformador asignarEstacion(@PathVariable Long id, @PathVariable Long estacionId) {
+        Transformador transformador = transformadorRepository.findById(id).orElse(null);
+        Estacion estacion = estacionRepository.findById(estacionId).orElse(null);
+        if (transformador != null && estacion != null) {
+            transformador.setEstacion(estacion);
+            transformadorRepository.save(transformador);
+        }
+        return transformador;
+    }
+    @DeleteMapping("/{id}/estaciones")
+    public Transformador desasignarEstacion(@PathVariable Long id) {
+        Transformador transformador = transformadorRepository.findById(id).orElse(null);
+        if (transformador != null) {
+            transformador.setEstacion(null);
+            transformadorRepository.save(transformador);
+        }
+        return transformador;
+    }
+
+
 }
